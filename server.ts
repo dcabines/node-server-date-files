3cdb31df9de0a0254e8a788710595b6ceab160f4
David Cabiness <dcabines@gmail.com>
2023-12-10 13:50:47 -0500

    add files

---
+++ server.ts
@@ -0,0 +1,50 @@
+import * as http from "http";
+import * as fs from "fs";
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const datePart = req.url?.replace('/', '').replace('.json', '') || null;
+
+  if(!datePart) {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
+
+  var date = new Date(datePart);
+
+  if(!date.getTime) {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
+
+  const now = new Date();
+
+  if(now.getTime() < date.getTime()) {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
+
+  const filePath = `./public/${req.url}`;
+  res.setHeader('Content-Type', 'application/json');
+
+  fs.readFile(filePath, (err, content) => {
+    if(err) {
+      res.statusCode = 404;
+      res.end();
+      return;
+    }
+
+    res.statusCode = 200;
+    res.end(content, 'utf-8');
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
